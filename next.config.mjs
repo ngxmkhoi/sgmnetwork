acd9db50a9a6661ca47edc6aeba1eb25af742dd9
VFuture <blazehunter01062008@gmail.com>
2026-04-05 14:08:14 +0700

    fix: them dl.gmc.freefiremobile.com vao next image domains

--- next.config.mjs
+++ next.config.mjs
@@ -24,6 +24,10 @@ const nextConfig = {
       },
       {
         protocol: "https",
+        hostname: "dl.gmc.freefiremobile.com",
+      },
+      {
+        protocol: "https",
         hostname: "upload.wikimedia.org",
       },
       {
